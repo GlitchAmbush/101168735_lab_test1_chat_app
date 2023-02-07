58ef4d27bc0f9061b6f9f61667187320dbeec6e1
Lemuel Javier <lemmytheleo@yahoo.com>
2023-02-07 12:36:52 -0500

    Attempted integration of User into chat message display.

--- models/PrivateMessage.js
+++ models/PrivateMessage.js
@@ -1,14 +1,14 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 const PrivateMessageSchema = new mongoose.Schema({
-    from_user: {type: String},
-    to_user: {type: String},
-    message: {type: String},
-    date_sent: {type: String}
+  from_user: { type: String, required: true },
+  to_user: { type: String, required: true },
+  message: { type: String },
+  date_sent: { type: String },
 });
 
-PrivateMessageSchema.post('init', (doc) => {
-    console.log('%s has been initialized from the db', doc._id);
+PrivateMessageSchema.post("init", (doc) => {
+  console.log("%s has been initialized from the db", doc._id);
 });
 
 const PrivateMessage = mongoose.model("private_messages", PrivateMessageSchema);
--- server.js
+++ server.js
@@ -1,51 +1,70 @@
-const path = require('path');
-const http = require('http');
-const express = require('express');
-const mongoose = require('mongoose');
-const socketio = require('socket.io');
+const path = require("path");
+const http = require("http");
+const express = require("express");
+const mongoose = require("mongoose");
+const socketio = require("socket.io");
 
 const app = express();
 const server = http.createServer(app);
 const io = socketio(server);
 
-const messageRouter = require('./routes/MessageRoutes.js')
-const formatMessage = require('./utils/message.js')
+const messageRouter = require("./routes/MessageRoutes.js");
+const formatMessage = require("./utils/message.js");
+const { userJoin, getCurrentUser } = require("./utils/currentUsers.js");
 
 // mongoDB connection
-mongoose.connect('mongodb+srv://scrungle:pwxTTNE0tpv8pwZS@cluster0.mfenz3g.mongodb.net/labtest1?retryWrites=true&w=majority', {
-    useNewUrlParser: true,
-    useUnifiedTopology: true
-}).then(success => {
-    console.log('Success Mongodb connection')
-}).catch(err => {
-    console.log('Error Mongodb connection')
-});
+mongoose
+  .connect(
+    "mongodb+srv://scrungle:pwxTTNE0tpv8pwZS@cluster0.mfenz3g.mongodb.net/labtest1?retryWrites=true&w=majority",
+    {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    }
+  )
+  .then((success) => {
+    console.log("Success Mongodb connection");
+  })
+  .catch((err) => {
+    console.log("Error Mongodb connection: ", err);
+  });
 
-app.use('/static', express.static(path.join(__dirname, 'public/js')))
+app.use("/static", express.static(path.join(__dirname, "public/js")));
 
-app.use(messageRouter)
+app.use(messageRouter);
 
-const botName = 'System'
+const botName = "System";
 
 // run when user connections
-io.on('connection', socket => {
+io.on("connection", (socket) => {
+  socket.on("joinRoom", ({ username, room }) => {
+    const user = userJoin(socket.id, username, room);
+
+    socket.join(user.room);
+
     // current user connect
-    socket.emit('message', formatMessage(botName, 'Welcome!'))
+    socket.emit("message", formatMessage(botName, "Welcome!"));
 
     // user connect
-    socket.broadcast.emit('message', formatMessage(botName, 'A user has connected'))
+    socket.broadcast
+      .to(user.room)
+      .emit(
+        "message",
+        formatMessage(botName, `${user.username} has connected`)
+      );
 
     // user disconnect
-    socket.on('disconnect', () => {
-        io.emit('message', formatMessage(botName, 'User has disconnected'))
+    socket.on("disconnect", () => {
+      io.emit("message", formatMessage(botName, "User has disconnected"));
     });
 
     // listen for message
-    socket.on('chatMessage', (msg) => {
-        io.emit('message', formatMessage('USER', msg))
-    })
+    socket.on("chatMessage", (msg) => {
+      const user = getCurrentUser(socket.id);
+      io.to(user.room).emit("message", formatMessage(user.username, msg));
+    });
+  });
 });
 
 server.listen(3000, () => {
-    console.log(`Server is running on Port 3000`);
+  console.log(`Server is running on Port 3000`);
 });
